e4d0725b4294ac60d06c888b38bc34eb71fdfc77
Simon Eiber <eiber.simon@web.de>
2025-04-09 16:34:27 +0200

    removed obsolete code

--- src/routes/api.py
+++ src/routes/api.py
@@ -1,31 +1,11 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 
-from src.routes import graph, select
-from src.api.GenreMap import GenreMap
+from src.routes import graph
 
 router = APIRouter(prefix="/api", default_response_class=JSONResponse)
 
 router.include_router(graph.router)
 
-router.include_router(select.router)
-
-genre_map = GenreMap()
-
-@router.get("/genre/{genre_id}")
-async def get_subgenres(genre_id: int):
-  genre_map.add_genre(genre_id)
-  return genre_map.get_json()
-
-@router.get("/subgenres/{genre_id}")
-async def get_subgenres(genre_id: int):
-  genre_map.add_subgenres(genre_id)
-  return genre_map.get_json()
-
-@router.get("/reset")
-async def reset():
-  global genre_map
-  genre_map = GenreMap()
-  return genre_map.get_json()
 
 
--- src/routes/graph.py
+++ src/routes/graph.py
@@ -3,20 +3,14 @@ from fastapi.responses import JSONResponse
 
 from src.models.clientData import GraphUpdate
 from src.models.create_SessionResponse import create_SessionResponse
-from src.models.graph_interaction import toggle_genre_selection
-from src.api.GenreMap import GenreMap
 from src.models.GenreDisplayStrategy import StartingGenresStrategy
 from src.models.SessionData import SessionData, GenreData
 from src.services.session_manager import get_req_session, store_session
 
 router = APIRouter(prefix="/graph", default_response_class=JSONResponse)
 
-genre_map = GenreMap()
 
 
-def add_data_to_json(json, session: SessionData):
-  json["data"] = session.genres
-  return json
 
 @router.post("/update")
 async def update_graph(request: GraphUpdate, session: SessionData = Depends(get_req_session)):
@@ -47,53 +41,5 @@ async def get_current_graph(request: Request):
   session = await get_req_session(request)
   response = create_SessionResponse(session)
   return response
-
-@router.get("/initial_graph")
-async def get_initial_graph(request: Request):
-  session = await get_req_session(request)
-  layout = StartingGenresStrategy().to_json(session)
-  json = add_data_to_json(layout, session)
-  return json
-
-@router.get("/genre/{genre_id}")
-async def select_genre(request: Request, genre_id: int):
-  session = await get_req_session(request)
-  session = toggle_genre_selection(session, genre_id)
-  await store_session(session)
-
-  layout = StartingGenresStrategy().to_json(session)
-  json = add_data_to_json(layout, session)
-  return json
-
-
-@router.get("/subgenres/{genre_id}")
-async def get_subgenres(request: Request, genre_id: int):
-  session = await get_req_session(request)
-  session = toggle_genre_expansion(session, genre_id)
-  await store_session(session)
-
-  layout = StartingGenresStrategy().to_json(session)
-  json = add_data_to_json(layout, session)
-  return json
-
-@router.get("/collapse_all")
-async def collapse_all(request: Request):
-  session = await get_req_session(request)
-  session.genres.expanded = []
-  await store_session(session)
-
-  layout = StartingGenresStrategy().to_json(session)
-  json = add_data_to_json(layout, session)
-  return json
-
-@router.get("/reset")
-async def reset(request: Request):
-  session = await get_req_session(request)
-  session.genres = GenreData()
-  await store_session(session)
-
-  layout = StartingGenresStrategy().to_json(session)
-  json = add_data_to_json(layout, session)
-  return json
 
 
