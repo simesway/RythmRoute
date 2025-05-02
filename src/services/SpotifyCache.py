c0ef68033e7d207bc8724b3c3d8e77b3e6029c28
Simon Eiber <eiber.simon@web.de>
2025-05-02 10:16:09 +0200

    refactored spotify object caching

--- src/services/SpotifyCache.py
+++ src/services/SpotifyCache.py
@@ -1,15 +1,16 @@
 import json
 from enum import Enum
 from pydantic import BaseModel
-from typing import List, Optional
-
+from typing import List, Optional, Type, TypeVar
 from src.services.redis_client import redis_sync
 from src.services.spotify_client import SpotifyClient
 
 
-TOP_TRACKS_CACHE_TIME = 86400 # 1 DAY
-ALBUMS_CACHE_TIME = 2592000 # 30 DAYS
-ALBUM_TRACKS_CACHE_TIME = 2592000 # 30 DAYS
+class CacheConfig:
+  SINGLE_OBJECT_TTL = 86400  # 1 day
+  TOP_TRACKS_TTL = 86400
+  ALBUMS_TTL = 86400
+  ALBUM_TRACKS_TTL = 86400
 
 
 class AlbumType(Enum):
@@ -18,14 +19,28 @@ class AlbumType(Enum):
   EP = 'ep'
   SINGLE = 'single'
 
+
+class SpotifyModel(BaseModel):
+  id: str
+  name: str
+
+  def __repr__(self):
+    return self.name
+
+  def __eq__(self, other) -> bool:
+    return isinstance(other, self.__class__) and self.id == other.id
+
+  def __hash__(self) -> int:
+    return hash(self.id)
+
+
 class ImageObject(BaseModel):
   url: str
   width: Optional[int] = None
   height: Optional[int] = None
 
-class Album(BaseModel):
-  id: str
-  name: str
+
+class Album(SpotifyModel):
   type: AlbumType
   release_date: str
   total_tracks: int
@@ -35,90 +50,148 @@ class Album(BaseModel):
   class Config:
     use_enum_values = True
 
-class Track(BaseModel):
-  id: str
-  name: str
+
+class Track(SpotifyModel):
   album_id: str
   duration: int
   popularity: Optional[int]
   artist_ids: List[str]
 
 
-class SpotifyCache:
-  def __init__(self):
-    self.r = redis_sync
-    self.sp = SpotifyClient().get_spotify_client()
+class Artist(SpotifyModel):
+  popularity: Optional[int]
+  genres: List[str] = []
+  images: List[ImageObject] = []
 
+
+class DataConverter:
   @staticmethod
-  def _convert_to_img_obj(images) -> List[ImageObject]:
+  def to_image_objects(images: List[dict]) -> List[ImageObject]:
     return [ImageObject(
-      url=i['url'],
-      width=i['width'] or None,
-      height=i['height'] or None
-    ) for i in images ]
+      url=img['url'],
+      width=img['width'] or None,
+      height=img['height'] or None
+    ) for img in images]
 
   @staticmethod
-  def _convert_to_track(t: dict, album_id: str = None):
+  def to_track(track_data: dict, album_id: str = None) -> Track:
     return Track(
-      id=t['id'],
-      name=t['name'],
-      artist_ids=[a['id'] for a in t['artists']],
-      album_id=album_id or t['album']['id'],
-      duration=t['duration_ms'],
-      popularity=t.get('popularity', None)
+      id=track_data['id'],
+      name=track_data['name'],
+      artist_ids=[artist['id'] for artist in track_data['artists']],
+      album_id=album_id or track_data['album']['id'],
+      duration=track_data['duration_ms'],
+      popularity=track_data.get('popularity')
     )
 
   @staticmethod
-  def _serialize(items):
-    return json.dumps([i.model_dump() for i in items])
+  def to_album(album_data: dict) -> Album:
+    return Album(
+      id=album_data['id'],
+      name=album_data['name'],
+      type=album_data['album_type'],
+      total_tracks=album_data['total_tracks'],
+      release_date=album_data['release_date'],
+      artist_ids=[a['id'] for a in album_data['artists']],
+      images=DataConverter.to_image_objects(album_data['images'])
+    )
 
   @staticmethod
-  def _deserialize(cls, data: str):
+  def to_artist(artist_data: dict) -> Artist:
+    return Artist(
+      id=artist_data['id'],
+      name=artist_data['name'],
+      popularity=artist_data.get('popularity'),
+      genres=artist_data.get('genres', []),
+      images=DataConverter.to_image_objects(artist_data['images'])
+    )
+
+  @staticmethod
+  def serialize(items: List[BaseModel]) -> str:
+    return json.dumps([item.model_dump() for item in items])
+
+  @staticmethod
+  def deserialize(cls: Type[BaseModel], data: str) -> List[BaseModel]:
     return [cls.model_validate(item) for item in json.loads(data)]
 
-  def _get_data(self, cls, key):
-    data = self.r.get(key)
+
+class SpotifyCache:
+  def __init__(self):
+    self.redis = redis_sync
+    self.spotify = SpotifyClient().get_spotify_client()
+    self.converter = DataConverter()
+
+  def get_track(self, track_id: str) -> Track:
+    key = f"track:{track_id}"
+    data = self.redis.get(key)
+
     if data:
-      return self._deserialize(cls, data)
-    return None
+      return Track.model_validate_json(data)
+
+    track = self.spotify.track(track_id)
+    data = self.converter.to_track(track)
+    self.redis.setex(key, CacheConfig.SINGLE_OBJECT_TTL, data.model_dump_json())
+    return data
+
+  def get_album(self, album_id: str) -> Album:
+    key = f"album:{album_id}"
+    data = self.redis.get(key)
+
+    if data:
+      return Album.model_validate_json(data)
+
+    album = self.spotify.album(album_id)
+    data = self.converter.to_album(album)
+    self.redis.setex(key, CacheConfig.SINGLE_OBJECT_TTL, data.model_dump_json())
+    return data
+
+  def get_artist(self, artist_id: str) -> Artist:
+    key = f"artist:{artist_id}"
+    data = self.redis.get(key)
+
+    if data:
+      return Artist.model_validate_json(data)
+
+    artist = self.spotify.artist(artist_id)
+    data = self.converter.to_artist(artist)
+    self.redis.setex(key, CacheConfig.SINGLE_OBJECT_TTL, data.model_dump_json())
+    return data
 
   def get_top_tracks(self, artist_id: str) -> List[Track]:
     key = f"artist:top_tracks:{artist_id}"
-    data = self._get_data(Track, key)
+    data = self.redis.get(key)
 
-    if not data:
-      tracks = self.sp.artist_top_tracks(artist_id)['tracks']
-      data = [self._convert_to_track(t) for t in tracks]
+    if data:
+      return self.converter.deserialize(Track, data)
 
-      self.r.setex(key, TOP_TRACKS_CACHE_TIME, self._serialize(data))
+    tracks = self.spotify.artist_top_tracks(artist_id)['tracks']
+    data = [self.converter.to_track(t) for t in tracks]
+    self.redis.setex(key, CacheConfig.TOP_TRACKS_TTL, self.converter.serialize(data))
     return data
 
   def get_releases(self, artist_id: str) -> List[Album]:
     key = f"artist:releases:{artist_id}"
-    data = self._get_data(Album, key)
+    data = self.redis.get(key)
 
-    if not data:
-      albums = self.sp.artist_albums(artist_id, include_groups='album,compilation,single,ep')['items']
-      data = [Album(
-        id=a['id'],
-        name=a['name'],
-        type=a['album_type'],
-        total_tracks=a['total_tracks'],
-        release_date=a['release_date'],
-        artist_ids=[a['id'] for a in a['artists']],
-        images=self._convert_to_img_obj(a['images'])
-      ) for a in albums]
+    if data:
+      return self.converter.deserialize(Album, data)
 
-      self.r.setex(key, ALBUMS_CACHE_TIME, self._serialize(data))
+    albums = self.spotify.artist_albums(
+      artist_id,
+      include_groups='album,compilation,single,ep'
+    )['items']
+    data = [self.converter.to_album(a) for a in albums]
+    self.redis.setex(key, CacheConfig.ALBUMS_TTL, self.converter.serialize(data))
     return data
 
   def get_album_tracks(self, album_id: str) -> List[Track]:
     key = f"album:tracks:{album_id}"
-    data = self._get_data(Album, key)
+    data = self.redis.get(key)
 
-    if not data:
-      tracks = self.sp.album_tracks(album_id)['items']
-      data = [self._convert_to_track(t, album_id) for t in tracks]
+    if data:
+      return self.converter.deserialize(Track, data)
 
-      self.r.setex(key, ALBUM_TRACKS_CACHE_TIME, self._serialize(data))
+    tracks = self.spotify.album_tracks(album_id)['items']
+    data = [self.converter.to_track(t, album_id) for t in tracks]
+    self.redis.setex(key, CacheConfig.ALBUM_TRACKS_TTL, self.converter.serialize(data))
     return data
