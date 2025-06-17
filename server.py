e4f438b29dc1ec036686165f177879f12e6eaea5
Simon Eiber <eiber.simon@web.de>
2025-06-17 18:37:23 +0200

    removed redundant router

--- server.py
+++ server.py
@@ -1,15 +1,13 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from src.routes import api, spotify, main, session
+from src.routes import api, spotify, main
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="src/static"), name="static")
 
 app.include_router(main.router)
-
-app.include_router(session.router)
 
 app.include_router(api.router)
 
