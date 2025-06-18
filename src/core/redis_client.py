8a83ca1918b6eb22f7560347b4ad8bc45968ee60
Simon Eiber <eiber.simon@web.de>
2025-06-18 16:28:29 +0200

    added env variables for redis

--- src/core/redis_client.py
+++ src/core/redis_client.py
@@ -1,4 +1,5 @@
 import redis
+from src.config import REDIS_HOST, REDIS_PORT
 
-redis_client = redis.asyncio.from_url("redis://localhost:6379", decode_responses=True)
-redis_sync = redis.Redis(host="localhost", port=6379, decode_responses=True)
+redis_client = redis.asyncio.from_url(f"redis://{REDIS_HOST}:{REDIS_PORT}", decode_responses=True)
+redis_sync = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
